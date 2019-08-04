2c49f2b30d020c4e85115609563e7dedd580e693
Olivier TUYISHIME <otuyishime@bk.rw>
2019-08-04 17:14:50 +0200

    added cypress for end to end test

--- client/components/Hero/Hero.js
+++ client/components/Hero/Hero.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => (
-    <div className="w-full h-64 flex items-center justify-center">
-        <h1 className="text-xl text-center mx-5 lg:text-5xl font-bold">
-            FULLSTACK-JS MERN STARTER
-        </h1>
-    </div>
-)
+  <div className="w-full h-64 flex items-center justify-center">
+    <h1
+      id="home-page-title"
+      className="text-xl text-center mx-5 lg:text-5xl font-bold"
+    >
+      FULLSTACK-JS MERN STARTER
+    </h1>
+  </div>
+);
 
-export default Hero
+export default Hero;
